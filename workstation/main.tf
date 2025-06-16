b7e1eed87b797c23b7ee7db27ca99bb6bba0d2eb
bibek73 <bibek@capitalnumbers.com>
2025-06-16 12:05:24 +0530

    added

--- workstation/main.tf
+++ workstation/main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_version = ">= 1.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "dev/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
-  }
-}
-
-provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
-}
-
 module "account_setup" {
   source        = "./modules/account-setup"
   count         = var.enable_account_setup ? 1 : 0
